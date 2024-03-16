c07ba7974b61232bd7e931099da8582428ba1347
rabindra789 <rabindrameher116@gmail.com>
2024-03-16 23:25:26 +0530

    Study about varibles

---
+++ 01_basic/01_varibles.js
@@ -0,0 +1,20 @@
+const accountId = 1803
+let accountEmail = "rabindrameher116@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bargarh"  // Not recommanded
+let accountState;
+
+// accountId = 2  //Not allowed
+
+
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "984163"
+accountCity = "Bhubaneswar"
+
+
+// Prefer not to use var because of the issue in block scope and functional scope.
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
