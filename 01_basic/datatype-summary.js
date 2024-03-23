b1aab4b8d3e82f5788404196484fbe25e50e1acb
rabindra789 <rabindrameher116@gmail.com>
2024-03-23 20:32:36 +0530

    stack and heap memory

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -52,3 +52,27 @@ const myFunction = function () {
 
 
 //  Link - https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack Memory (Primitive) and Heap Memory (Non-Primitive)
+
+let myYoutubeNeme = "Rabindradotcom";
+
+anotherName = "codewithrabindra";
+console.log(myYoutubeNeme);
+console.log(anotherName);
+
+let user1 = {
+  email: "user1@gmail.com",
+  upi: "user1@ybl"
+}
+
+let user2 = user1;
+
+user2.email = "user2@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
+
+// 
