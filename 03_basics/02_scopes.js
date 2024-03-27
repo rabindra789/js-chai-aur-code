cd856c54b5360648cd6469ac810fb860ce767cf4
rabindra789 <rabindrameher116@gmail.com>
2024-03-27 19:57:12 +0530

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,13 +1,53 @@
 // var c = 300
 
-let a = 300
+// let a = 300
+
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   console.log("inner: ", a);
+// }
+
+// console.log(a);
+// console.log(b);
+
+function one() {
+  const username = "rabindra";
+
+  function two() {
+    const website = "google";
+    console.log(username);
+  }
+  // console.log(website);
+
+  two();
+}
+
+// one();
+
 
 if (true) {
-  let a = 10;
-  const b = 20;
-  console.log("inner: ", a);
+  const username = "rabindra"
+  if (username === "rabindra") {
+    const website = " youtube"
+    // console.log(username + website);
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(5))
+function addOne(num){
+    return ++num
 }
 
 
-console.log(a);
-// console.log(b);
+addTwo(5)
+const addTwo = function(num){
+  return num + 2
+}
