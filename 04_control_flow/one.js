095895c4d9aaff019d97af2623af1441d6bfdd9d
rabindra789 <rabindrameher116@gmail.com>
2024-03-28 23:28:38 +0530

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if
+const isUserLoggedIn = true
+const temp = 41
+
+// if (temp === 40) {
+//     console.log("less than 50");
+// }else {
+//     console.log("temp is greater than 40");
+// }
+
+// console.log("execute");
+
+// <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// // if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than");
+// }else if (balance < 750) {
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 900");
+// }else {
+//     console.log("less than 1200");
+// }
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to shoping");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "mar"
+
+switch (month) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "mar":
+        console.log("Mar");
+        break;
+    case "apr":
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("defaulty case match");
+        break;
+}
